1aeabfc4eb15ca7412ba0b2a5b4a5ea2e42fa3f0
Rebecca Wollard <rbwollard@gmail.com>
2022-12-16 20:47:46 -0700

    commit tree e3f03bb83c14bd0240691387e5095b48526d5bd4

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickupLocation) {
+    return Math.abs(pickupLocation - 42)
+}
+
+function distanceFromHqInFeet(start, destination) {
+    return distanceFromHqInBlocks (start) * 264
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs ((start - destination) * 264)
+}
+
+function calculatesFarePrice (start, destination) {
+    const totalDistance = distanceTravelledInFeet(start, destination)
+    if (totalDistance <= 400) {
+        return 0
+    } else if (totalDistance > 400 && totalDistance <= 2000) {
+        return (totalDistance - 400)*.02
+    } else if (totalDistance >2000 && totalDistance <= 2500) {
+        return 25
+         } else {
+                return 'cannot travel that far'
+            }
+        } 
+    
